9fe6e9668e74ffb7f07e60eee52eb9dcb9a5d64b
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-01-30 15:43:37 +0100

    change format in offset file

--- movement/offset.py
+++ movement/offset.py
@@ -1,30 +1,29 @@
-# Offsets are used to change the position of a coordenate
-# by adjusting a parameter and not writting the same point 
-# changing only one parameter
-
 from pyniryo import *
 
+# Offsets are used to change the position of a coordenate
+# by adjusting a parameter and not writting the same point
+# changing only one parameter
 
 # CONSTANS
 IP_ADDRESS = "10.10.10.10"
-
-
-# CONNECTION
 robot = NiryoRobot(IP_ADDRESS)
-
 
 pick_pose = PoseObject(
     x=0.30, y=0.0, z=0.15,
     roll=0, pitch=1.57, yaw=0.0
 )
-first_place_pose = PoseObject(
-    x=0.0, y=0.2, z=0.15,
-    roll=0, pitch=1.57, yaw=0.0
-)
 
-for i in range(8):
-    print("i = ", i)
-    robot.move_pose(pick_pose)
-    print("OFFSET")
-    new_place_pose = first_place_pose.copy_with_offsets(x_offset=0.05 * i)
-    robot.move_pose(new_place_pose)
+max_catch_count = 4
+catch_count = 0
+offset_size = 0.05
+
+while catch_count > max_catch_count:
+
+    next_place_pose = pick_pose.copy_with_offsets(
+        x_offset=catch_count * offset_size,
+        # y_offset=catch_count * offset_size,
+        # z_offset=catch_count * offset_size
+    )
+    robot.place_from_pose(next_place_pose)
+
+robot.go_to_sleep()
