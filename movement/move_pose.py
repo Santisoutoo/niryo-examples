c6f7620ad862aef7bebdcfe59058106258136b53
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-01-30 15:28:49 +0100

    updating move_pose format

--- movement/move_pose.py
+++ movement/move_pose.py
@@ -1,10 +1,7 @@
 from pyniryo import *
 
-# CONSTANS
-IP_ADDRESS = "10.10.10.10"
-
-
 # CONNECTION
+IP_ADDRESS = "10.10.10.10"
 robot = NiryoRobot(IP_ADDRESS)
 
 
@@ -13,21 +10,26 @@ robot = NiryoRobot(IP_ADDRESS)
 # list of 6 floats [x, y, z, roll, pitch, yaw]
 # A PoseObject which stores the values "x, y, z, roll, pitch, yaw"
 
-
+           # List examples
 pose_target = [0.2, 0.0, 0.3, 0.0, 0.0, 0.0]
 pose_target_obj = PoseObject(0.2, 0.0, 0.2, 0.0, 0.0, 0.0)
 
+             # FUNCTION
 print("MOVING WITH POSE FUNCTION AND FLOATS")
 robot.move_pose(0.2, 0.0, 0.2, 0.0, 0.0, 0.0)
+
 print("MOVING WITH POSE FUNCTION AND LIST")
 robot.move_pose(pose_target)
+
 print("MOVING WITH POSE FUNCTION AND POSE OBJET")
 robot.move_pose(pose_target_obj)
 
-# Moving Pose with setter
+              # SETTER
 print("MOVING WITH POSE SETTER AND FLOATS")
 robot.pose = (0.2, 0.0, 0.2, 0.0, 0.0, 0.0)
+
 print("MOVING WITH POSE SETTER AND LIST")
 robot.pose = pose_target
+
 print("MOVING WITH POSE SETTER AND POSE OBJET")
 robot.pose = pose_target_obj
