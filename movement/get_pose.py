2f02f8ac56b10b1ab229e9e41e8d4692c682e6f6
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-01-30 15:24:10 +0100

    actualizar formato get_pose

--- movement/get_pose.py
+++ movement/get_pose.py
@@ -1,14 +1,15 @@
 from pyniryo import *
 
 # CONNECTION
-robot = NiryoRobot('10.10.10.10')
+IP_ADDRESS = "10.10.10.10"
+robot = NiryoRobot(IP_ADDRESS)
 
 # It's possible to read axis postion in two ways
 # With a function or via getter
 
-# Getting Joints with function
+# Function
 pose_read = robot.get_pose()
-# Getting Joints with getter
+# Getter
 pose_read_2 = robot.pose
 
 print(pose_read)
@@ -30,13 +31,13 @@ pose_object_test = PoseObject(
 )
 
 # print(pose_object_test)
-# if we only want the values of the axis, 
+# if we only want the values of the axis,
 # we may use the function bellow
 
 # print(pose_object_test.to_list())
 
 # output: [0.1402, -0.0001, 0.2033, 0.003, 0.751, -0.001]
-# If we compare this to __str__ , setters and fuction methods 
-# output: x = 0.1402, y = -0.0001, z = 0.2033 
+# If we compare this to __str__ , setters and fuction methods
+# output: x = 0.1402, y = -0.0001, z = 0.2033
 #         roll = 0.003, pitch = 0.751, yaw = -0.001
 # it is clearly much easier to operate with this data structre
