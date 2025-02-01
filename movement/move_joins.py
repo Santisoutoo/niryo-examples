f6226c3e4bdddff04e5603a7a5f06334e1afd439
Santiago <149386030+Santisoutoo@users.noreply.github.com>
2025-02-01 13:00:39 +0100

    updating commnets

--- movement/move_joins.py
+++ movement/move_joins.py
@@ -1,6 +1,6 @@
 from pyniryo import *
 
-# CONSTANS
+# CONSTANTS
 IP_ADDRESS = "10.10.10.10"
 POSITION = [-0.15, 0.3, 0.2, 0.3, -0.6, 0.0]
 POSITION_2 = [-0.5, -0.6, 0.0, 0.3, 0.0, 0.0]
@@ -8,24 +8,23 @@ POSITION_2 = [-0.5, -0.6, 0.0, 0.3, 0.0, 0.0]
 # CONNECTION
 robot = NiryoRobot(IP_ADDRESS)
 
+# It is possible to provide either 6 float numbers or a list of 6 floats to .move_joints()
 
-# It is posible to provide 6 float numbers to
-# .move_joins()
-# or a list o 6 floats
-
-print("MOVING JOINS WITH SETTERS AND FLOATS")
-# Moving Joints with setter & a list of floats
+print("MOVING JOINTS WITH SETTERS AND FLOATS")
+# Moving Joints using the setter with individual floats
 robot.joints = 0.2, -0.4, 0.0, 0.0, 0.0, 0.0
-print("MOVING JOINS WITH SETTERS AND LIST")
+print("MOVING JOINTS WITH SETTERS AND LIST")
+# Moving Joints using the setter with a predefined list of floats
 robot.joints = POSITION
 
-# # Moving Joints with function & 6 floats
-print("MOVING JOINS WITH FUNCTION AND FLOATS")
-# Moving Joints with function & a list of floats
+# Moving Joints using the function with individual floats
+print("MOVING JOINTS WITH FUNCTION AND FLOATS")
 robot.move_joints(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-print("MOVING JOINS WITH SETTERS AND LIST")
-# Moving Joints with setter & 6 floats
+print("MOVING JOINTS WITH FUNCTION AND LIST")
+# Moving Joints using the function with a predefined list of floats
 robot.move_joints(POSITION_2)
 
+# As shown in the get_pose.py file, you can use a Pose object and its to_list() method
+# to convert the pose into a list. This list can then be used directly in move_joints().
 
 robot.close_connection()
